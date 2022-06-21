388a002a866a0c5552fc40b3cc1a95d427a68a24
AlexJW <justWalsdi@protonmail.com>
2022-06-22 00:08:49 +0300

    Explicit conversion

--- hello.go
+++ hello.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+	explicit_conversion()
+}
+
+func explicit_conversion() {
+	var x int = 10
+	var y float64 = 30.2
+	var z float64 = float64(x) - y
+	var d int = x + int(y)
+	fmt.Println("Z =", z, "\nD =", d)
 }
